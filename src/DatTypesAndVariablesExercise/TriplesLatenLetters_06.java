01b6878a34e3709feaa5a4cc73c6e10cdc7e0c77
stoyanMollov <mollov9@gmail.com>
2022-09-27 15:07:10 +0300

    new

---
+++ src/DatTypesAndVariablesExercise/TriplesLatenLetters_06.java
@@ -0,0 +1,11 @@
+package DatTypesAndVariablesExercise;
+
+import java.util.Scanner;
+
+public class TriplesLatenLetters_06 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
