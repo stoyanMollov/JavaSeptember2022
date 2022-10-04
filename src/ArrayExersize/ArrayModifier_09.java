aa986b71524cd0bc0c62adf3c3b9d994a27b387b
Stoyan Mollov <mollov9@gmail.com>
2022-10-04 23:10:07 +0300

    ArrayModifier

--- src/ArrayExersize/ArrayModifier_09.java
+++ src/ArrayExersize/ArrayModifier_09.java
@@ -13,13 +13,43 @@ public class ArrayModifier_09 {
 
         while(!command.equals("end")){
             if (command.contains("swap")){
+                int index1 = Integer.parseInt(command.split(" ")[1]);
+                int index2 = Integer.parseInt(command.split(" ")[2]);
+
+                int element1 = numbers[index1];
+                int element2 = numbers[index2];
+
+                numbers[index1] = element2;
+                numbers[index2] = element1;
 
             } else if (command.contains("multiply")) {
                 //multiply
+                int index1 = Integer.parseInt(command.split(" ")[1]);
+                int index2 = Integer.parseInt(command.split(" ")[2]);
+
+                int element1 = numbers[index1];
+                int element2 = numbers[index2];
+
+                int product = element1 * element2;
+                numbers[index1] = product;
+
+
             } else if (command.equals("decrease")) {
+                for (int index = 0; index <= numbers.length - 1; index++) {
+                    numbers[index]--;
+                }
                 //decrease
             }
             command = scan.nextLine();
+        }
+        for (int index = 0; index <= numbers.length - 1; index++) {
+            int currentNumber = numbers[index];
+
+            if(index != numbers.length - 1){
+                System.out.print(currentNumber + ", ");
+            }else{
+                System.out.print(currentNumber);
+            }
         }
     }
 }
