fe45f7c951ff00dfa66f44519fd96b015f511ca4
stoyanMollov <mollov9@gmail.com>
2022-10-09 14:15:57 +0300

    PascalTriangle_02

---
+++ src/MoreExerciseArrays/PascalTriangle_02.java
@@ -0,0 +1,20 @@
+package MoreExerciseArrays;
+
+import java.util.Scanner;
+
+public class PascalTriangle_02 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        long n = Integer.parseInt(scan.nextLine());
+
+        for (long line = 1; line <= n; line++) {
+            long C = 1;
+            for(long i = 1; i <= line; i++) {
+                System.out.print(C + " ");
+                C = C * (line - i) / i;
+            }
+            System.out.println();
+        }
+    }
+}
