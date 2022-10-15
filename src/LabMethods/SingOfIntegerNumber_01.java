954ea5e68f71d07daa277889f8f0757155216c96
Stoyan Mollov <mollov9@gmail.com>
2022-10-15 12:48:54 +0300

    SignOfIntegerNumber_01

---
+++ src/LabMethods/SingOfIntegerNumber_01.java
@@ -0,0 +1,23 @@
+package LabMethods;
+
+import java.util.Scanner;
+
+public class SingOfIntegerNumber_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numInput = Integer.parseInt(scanner.nextLine());
+
+        printNumbersSign(numInput);
+    }
+
+    public static void printNumbersSign(int num){
+        if (num > 0){
+            System.out.printf("The number %d is positive.",num);
+        } else if (num < 0) {
+            System.out.printf("The number %d is negative.",num);
+        }else {
+            System.out.printf("The number %d is zero.",num);
+        }
+    }
+}
